19edfdcc530b7fcdee2e0058388ea98e43f71df9
danhale-git <danhale87@gmail.com>
2020-12-11 09:41:41 +0000

    Server as a Reader Writer.

--- internal/server/server.go
+++ internal/server/server.go
@@ -8,8 +8,6 @@ import (
 	"os"
 	"strconv"
 
-	"golang.org/x/crypto/ssh/terminal"
-
 	docker "github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
@@ -26,8 +24,30 @@ const (
 )
 
 type Server struct {
+	cli *docker.HijackedResponse
+
 	// Terminal io to docker container bedrock_server process
 	// Container things
+}
+
+func (s *Server) Read(p []byte) (n int, err error) {
+	n, err = s.cli.Reader.Read(p)
+
+	if n > 2 {
+		log.Printf("Read %d bytes", n)
+	}
+
+	return
+}
+
+func (s *Server) Write(p []byte) (n int, err error) {
+	n, err = s.cli.Conn.Write(p)
+
+	if len(p) > 1 {
+		fmt.Printf("Wrote %d bytes: '%s'\n", n, p)
+	}
+
+	return
 }
 
 // docker run -d -e EULA=TRUE -p <HOST_PORT>:19132/udp danhaledocker/craftmine:<VERSION>
@@ -86,31 +106,28 @@ func Run(hostPort int, name string) error {
 		Stream: true,
 	})
 
+	server := Server{cli: &waiter}
+
 	if err != nil {
 		return err
 	}
 
-	go io.Copy(os.Stdout, waiter.Reader)
-	//go io.Copy(os.Stderr, waiter.Reader) // this causes an index out of range exception in bufio.go, presumably because the reader is being read from twice. It should probably be copied.
-	go io.Copy(waiter.Conn, os.Stdin)
+	go func() {
+		if _, err = io.Copy(os.Stdout, &server); err != nil {
+			panic(err)
+		}
+	}()
+
+	go func() {
+		if _, err = io.Copy(&server, os.Stdin); err != nil {
+			panic(err)
+		}
+	}()
 
 	// Start the container
 	err = c.ContainerStart(ctx, createResp.ID, docker.ContainerStartOptions{})
 	if err != nil {
 		return err
-	}
-
-	// Save terminal state
-	fd := int(os.Stdin.Fd())
-
-	var oldState *terminal.State
-
-	if terminal.IsTerminal(fd) {
-		oldState, err = terminal.MakeRaw(fd)
-		if err != nil {
-			return err
-		}
-		defer terminal.Restore(fd, oldState)
 	}
 
 	// Wait for the container to stop running
@@ -122,23 +139,6 @@ func Run(hostPort int, name string) error {
 		}
 	case <-statusCh:
 	}
-
-	// Restore terminal state
-	terminal.Restore(fd, oldState)
-
-	// Panics if AutoRemove=true
-	/*// Print container logs
-	out, err := c.ContainerLogs(ctx, createResp.ID, types.ContainerLogsOptions{ShowStdout: true})
-	if err != nil {
-		panic(err)
-	}
-
-	b, err := ioutil.ReadAll(out)
-	if err != nil {
-		return nil, err
-	}
-
-	fmt.Println(string(b))*/
 
 	return nil
 }
