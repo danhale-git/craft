bea9fe6295c45727c9faffa29567b1a7cae052cb
danhale-git <danhale87@gmail.com>
2020-12-29 20:52:53 +0000

    Files by name.

--- internal/files/files.go
+++ internal/files/files.go
@@ -3,11 +3,12 @@ package files
 import (
 	"os"
 	"path"
+	"sort"
 )
 
 func save(dirPath, fileName string, body []byte) error {
 	if _, err := os.Stat(dirPath); os.IsNotExist(err) {
-		err = os.MkdirAll(dirPath, 755)
+		err = os.MkdirAll(dirPath, 0755)
 		if err != nil {
 			return err
 		}
@@ -24,4 +25,25 @@ func save(dirPath, fileName string, body []byte) error {
 	}
 
 	return nil
+}
+
+type filesByName []os.FileInfo
+
+func (s filesByName) Len() int {
+	return len(s)
+}
+func (s filesByName) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+func (s filesByName) Less(i, j int) bool {
+	// Put i first
+	names := []string{s[i].Name(), s[j].Name()}
+	sort.Strings(names)
+
+	// i is still first
+	return names[0] == s[i].Name()
+}
+
+func SortFilesByName(files []os.FileInfo) {
+	sort.Sort(filesByName(files))
 }
