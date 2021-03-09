f5b6a94ab23e2e82e1cd3861e39bdb98a335b70b
danhale-git <danhale87@gmail.com>
2021-03-09 09:03:05 +0000

    set path variable

--- cmd/integration_tests.sh
+++ cmd/integration_tests.sh
@@ -2,5 +2,7 @@
 go install github.com/danhale-git/craft
 docker pull danhaledocker/craftmine:v1.7
 
-~/go/bin/craft run testserver1
-~/go/bin/craft list -a
+export PATH=$PATH:~/go/bin/
+
+craft run testserver1
+craft list -a
