9dc8f4545376fce1a3af9bc874539a3d45fac22a
danhale-git <danhale87@gmail.com>
2021-03-10 18:18:00 +0000

    Test configure command.

--- cmd/integration_tests.sh
+++ cmd/integration_tests.sh
@@ -4,10 +4,25 @@ docker pull danhaledocker/craftmine:v1.7
 
 export PATH=$PATH:~/go/bin/
 
+craft version
+
 craft run testserver
+
 craft list -a
+
+craft configure test --prop gamemode=creative
+
+mode=$(docker exec test cat /bedrock/server.properties | grep gamemode)
+if [ "$mode" != "gamemode=creative" ]; then
+  exit 1
+fi
+
 craft stop testserver
+
 craft start testserver
+
 craft backup testserver
+
 craft cmd testserver time set 0600
+
 craft stop testserver
