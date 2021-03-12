fc5a4f2a61444ea8ae27a8bc5c0b6655bf4eb442
danhale-git <danhale87@gmail.com>
2021-03-12 08:09:27 +0000

    Exit script on failed integration test.

--- cmd/integration_tests.sh
+++ cmd/integration_tests.sh
@@ -6,44 +6,56 @@ export PATH=$PATH:~/go/bin/
 
 # TODO: Remove sleep 2 when issue 40 is resolved: https://github.com/danhale-git/craft/issues/40
 sleep 2; echo "craft version"
-craft version
+if ! craft version; then
+  exit 1; fi
 
 sleep 2; echo "craft run testserver"
-craft run testserver
+if ! craft run testserver; then
+  exit 1; fi
 
-listOut=$(craft list)
+if ! listOut=$(craft list); then
+  exit 1; fi
 if [[ "$listOut" != "testserver   running - port 19132" ]]; then
   exit 1
 fi
 
 sleep 2; echo "craft configure testserver --prop gamemode=creative"
-craft configure testserver --prop gamemode=creative
+if ! craft configure testserver --prop gamemode=creative; then
+  exit 1; fi
 
-mode=$(docker exec testserver cat /bedrock/server.properties | grep gamemode)
+if ! mode=$(docker exec testserver cat /bedrock/server.properties | grep gamemode); then
+  exit 1; fi
 if [[ "$mode" != "gamemode=creative" ]]; then
   exit 1
 fi
 
 sleep 2; echo "craft stop testserver"
-craft stop testserver
+if ! craft stop testserver; then
+  exit 1; fi
 
 sleep 2; echo "craft list -a"
-listAllOut=$(craft list -a)
+if ! listAllOut=$(craft list -a); then
+  exit 1; fi
 if [[ $listAllOut != testserver* ]]; then
   exit 1
 fi
 
 sleep 2; echo "craft start testserver"
-craft start testserver
+if ! craft start testserver; then
+  exit 1; fi
 
 sleep 2; echo "craft backup testserver"
-craft backup testserver
+if ! craft backup testserver; then
+  exit 1; fi
 
 sleep 2; echo "craft cmd testserver time set 0600"
-craft cmd testserver time set 0600
+if ! craft cmd testserver time set 0600; then
+  exit 1; fi
 
 sleep 2; echo "craft export testserver -d ~"
-craft export testserver -d /
+if ! craft export testserver -d /; then
+  exit 1; fi
 
 sleep 2; echo "craft stop testserver"
-craft stop testserver
+if ! craft stop testserver; then
+  exit 1; fi
