3841ea20904d294764fb600f679fe1f798df1489
danhale-git <danhale87@gmail.com>
2021-03-12 08:23:14 +0000

    Fix export integration test.

--- cmd/integration_tests.sh
+++ cmd/integration_tests.sh
@@ -52,8 +52,10 @@ sleep 2; echo "craft cmd testserver time set 0600"
 if ! craft cmd testserver time set 0600; then
   exit 1; fi
 
+sudo mkdir /testmcworld; sudo chmod 0777 testmcworld
+
 sleep 2; echo "craft export testserver -d ~"
-if ! craft export testserver -d /; then
+if ! craft export testserver -d /testmcworld; then
   exit 1; fi
 
 sleep 2; echo "craft stop testserver"
